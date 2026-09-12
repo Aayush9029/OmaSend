7562d3cbf4b20efbc617366a5067d238c86cd3b6
Aayush9029 <contact@roguelabs.nyc>
2026-09-12 16:27:55 -0400

    Point the cask at the signed 0.2.2 macOS release

--- Casks/omasend.rb
+++ Casks/omasend.rb
@@ -1,6 +1,6 @@
 cask "omasend" do
-  version "0.2.1"
-  sha256 "080d043aafc5406338c520ee7b372f15644ef498c8806c938126d302ee622df0"
+  version "0.2.2"
+  sha256 "3cfe5f27d49ea5ab7ea0652e6a80b88d8d8f523d69180a1c496579a517a3576d"
 
   url "https://github.com/Aayush9029/OmaSend/releases/download/macos-v#{version}/OmaSend_#{version}_macOS_arm64.zip"
   name "OmaSend"
